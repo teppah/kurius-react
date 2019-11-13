ab82382fa24fbe9b1ec0a47d58821ec0896ab518
teppah <yifengyang01@gmail.com>
2019-11-13 11:53:13 -0500

    Updates

---
+++ precache-manifest.2e95728ac6fc376e0358fe1342323ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fd112fbeffafe63e8144c28be62ebe",
+    "url": "/kurius-react/index.html"
+  },
+  {
+    "revision": "c45fdfc66e3088369fad",
+    "url": "/kurius-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d29fda679e85d6402e7d",
+    "url": "/kurius-react/static/js/2.ad05d5aa.chunk.js"
+  },
+  {
+    "revision": "c45fdfc66e3088369fad",
+    "url": "/kurius-react/static/js/main.659a4dfd.chunk.js"
+  },
+  {
+    "revision": "1857e09ab2b611efeb9f",
+    "url": "/kurius-react/static/js/runtime-main.5a670ccf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/kurius-react/static/media/logo.25bf045c.svg"
+  }
+]);
