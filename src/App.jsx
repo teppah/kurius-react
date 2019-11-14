3bef13166ca75708dfff1af2131d000879d7ef73
Yi Feng Yang <yifengyang01@gmail.com>
2019-11-13 20:00:02 -0500

    Add photo gallery and angled gradient to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 // import logo from './logo.svg';
-// import './App.css';
+import "./App.css";
 import NavBar from "./components/NavBar";
 import KuriusHeading from "./components/KuriusHeading";
+import PhotoGallery from "./components/PhotoGallery";
+import AngledGradient from "./components/AngledGradient";
 import "semantic-ui-css/semantic.min.css";
 import { Container, Segment } from "semantic-ui-react";
 
 function App() {
   return (
     <Container fluid>
-      <NavBar />
-      <KuriusHeading />
+      <AngledGradient>
+        <NavBar />
+        <KuriusHeading />
+      </AngledGradient>
       <Container>
-        
+        <Segment>
+          <PhotoGallery />
+        </Segment>
       </Container>
     </Container>
   );
 }
+
+
 
 export default App;
