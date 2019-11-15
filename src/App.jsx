de9eed83518236dc28b8023639b1e24227324be0
Yi Feng Yang <yifengyang01@gmail.com>
2019-11-14 22:03:11 -0500

    Add to App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import KuriusHeading from "./components/KuriusHeading";
 import PhotoGallery from "./components/PhotoGallery";
 import AngledGradient from "./components/AngledGradient";
 import BigHeadline from "./components/BigHeadline";
+import InfoCard from "./components/InfoCard";
+import HackShowcase from "./components/HackShowcase";
 import "semantic-ui-css/semantic.min.css";
 import { Container, Segment, Grid } from "semantic-ui-react";
 
@@ -29,11 +31,36 @@ function App() {
           text="We are a start-up non-profit organization created by students for students. Our goal is to empower youth by making tech education and resources accessible to all."
         />
       </AngledGradient>
-      <Grid cetered>
-        <Grid.Column>
-          
-        </Grid.Column>
-      </Grid>
+      <Container style={{ marginTop: "8em", marginBottom: "8em" }}>
+        <Grid centered stackable verticalAlign={"middle"}>
+          <Grid.Row centered stretched>
+            <Grid.Column width={8} stretched>
+              <InfoCard
+                title="Kurius Capsules"
+                text="Specially designed for our students in mind, our Capsules gather a variety of industry specialists in a weekly workshop session. They have the unique chance to learn from the best in a tight-nit community of equally passionate students."
+              />
+            </Grid.Column>
+            <Grid.Column width={8} stretched>
+              <InfoCard
+                title="Kurius Periods"
+                text="Our periods allow our participants to collaborate in a fun and engaging session dedicated to coding, and work together on a big project that will be showcased at the end. "
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+      <Container fluid style={{ marginTop: "4em", marginBottom: "4em" }}>
+        <AngledGradient>
+          <BigHeadline
+            headline="What Is a Kurius Hack?"
+            text="Our program is a 5-week long 'hack' during which participants learn to code by tackling a specific problem in a guided workshop experience. They are held twice a week: once after school (Capsule) and once during the weekend (Period)."
+          />
+        </AngledGradient>
+      </Container>
+
+      <Container fluid style={{ marginTop: "4em", marginBottom: "4em" }}>
+        <HackShowcase />
+      </Container>
     </Container>
   );
 }
